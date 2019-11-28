54cc0adab0d7e477b60ff21d0a0f3de0159a5758
GuyCalloway <guyfcalloway@gmail.com>
2019-11-28 10:51:40 +0000

    Super REFACTOR using ITERTOOLS

--- max_prof.py
+++ max_prof.py
@@ -1,78 +1,11 @@
+from itertools import combinations
+
+
 class StockMasta:
     def __init__(self, input):
-        self.stocks2 = input
         self.stocks = input
-        self.min_index = []
-        self.max_index = []
-        self.min_index2 = []
-        self.max_index2 = []
 
-    def find_min_and_max(self):
-        arr = sort_array(self.stocks)
-        min = arr[0]
-        max = arr[-1]
-        while not self.index_check(self.min_index, self.max_index):
-            self.loop_thru_array(
-                self.stocks, self.min_index, self.max_index, min, max)
-            if self.stocks.index(min) > (len(self.stocks) - (self.stocks[::-1].index(max)+1)):
-                del self.stocks[self.stocks.index(min)]
-                del arr[0]
-                del self.min_index[0]
-                del self.max_index[0]
-                min = arr[0]
-        if self.index_check(self.min_index, self.max_index):
-            self.min_index = [self.min_index[0]]
-            self.max_index = [self.max_index[-1]]
-            return(arr[-1] - arr[0])
-
-    def find_min_and_max2(self):
-        arr = sort_array(self.stocks2)
-        min = arr[0]
-        max = arr[-1]
-        while not self.index_check(self.min_index2, self.max_index2):
-            self.loop_thru_array(
-                self.stocks2, self.min_index2, self.max_index2, min, max)
-            if self.stocks2.index(max) < self.stocks2.index(min):
-                del self.stocks2[self.stocks2.index(max)]
-                del arr[-1]
-                print(arr)
-                del self.min_index2[0]
-                del self.max_index2[0]
-                max = arr[-1]
-        if self.index_check(self.min_index2, self.max_index2):
-            self.min_index2 = [self.min_index2[0]]
-            self.max_index2 = [self.max_index2[-1]]
-            print(arr)
-            print(self.stocks2)
-            print(arr[-1])
-            print(arr[0])
-            return (arr[-1] - arr[0])
-
-    def loop_thru_array(self, array, min_index, max_index, min, max):
-        for i, x in enumerate(self.stocks):
-            if (x == max) and (i != 0):
-                max_index.append(i)
-            elif (x == min):
-                min_index.append(i)
-
-    def index_check(self, min_index, max_index):
-        for i in max_index:
-            for index in min_index:
-                if index - i < 0:
-                    return True
-        return False
-
-    def calculator(self):
-        x = self.find_min_and_max()
-        y = self.find_min_and_max2()
-        print("yyyyyyy")
-        print(y)
-        if x < y:
-            return y
-        else:
-            return x
-
-
-def sort_array(array1):
-    sorted_arr = sorted(array1)
-    return sorted_arr
+    def max_diff(self):
+        result = max(combinations(self.stocks, 2),
+                     key=lambda sub: sub[1]-sub[0])
+        return result[1] - result[0]
--- max_prof_test.py
+++ max_prof_test.py
@@ -4,23 +4,23 @@ from max_prof import StockMasta
 
 class TestStockMasta(unittest.TestCase):
 
-    def test_if_stockMasta_finds_min_and_max(self):
-        stocks = StockMasta([10, 7, 5, 8, 11, 9])
-        stocks.find_min_and_max()
-        self.assertEqual(stocks.min_index, [2])
-        self.assertEqual(stocks.max_index, [4])
+    # def test_if_stockMasta_finds_min_and_max(self):
+    #     stocks = StockMasta([10, 7, 5, 8, 11, 9])
+    #     stocks.find_min_and_max()
+    #     self.assertEqual(stocks.min_index, [2])
+    #     self.assertEqual(stocks.max_index, [4])
 
-    def test_if_stockMasta_finds_max_difference_before(self):
-        stocks = StockMasta([10, 11, 12, 1, 2, 11, 10])
-        stocks.find_min_and_max()
-        self.assertEqual(stocks.min_index, [0])
-        self.assertEqual(stocks.max_index, [2])
+    # def test_if_stockMasta_finds_max_difference_before(self):
+    #     stocks = StockMasta([10, 11, 12, 1, 2, 11, 10])
+    #     stocks.find_min_and_max()
+    #     self.assertEqual(stocks.min_index, [0])
+    #     self.assertEqual(stocks.max_index, [2])
 
-    def test_if_stockMasta_finds_max_difference_after(self):
-        stocks = StockMasta([10, 11, 12, 1, 2, 11, 10])
-        stocks.find_min_and_max2()
-        self.assertEqual(stocks.min_index2, [1])
-        self.assertEqual(stocks.max_index2, [3])
+    # def test_if_stockMasta_finds_max_difference_after(self):
+    #     stocks = StockMasta([10, 11, 12, 1, 2, 11, 10])
+    #     stocks.find_min_and_max2()
+    #     self.assertEqual(stocks.min_index2, [1])
+    #     self.assertEqual(stocks.max_index2, [3])
 
     # def test_if_stockMasta_finds_max_difference_before2(self):
     #     stocks = StockMasta([10, 10, 10, 10, 10, 10, 10])
@@ -35,11 +35,11 @@ class TestStockMasta(unittest.TestCase):
 
     def test_if_stockMasta_finds_max(self):
         stocks = StockMasta([10, 11, 12, 1, 2, 11, 10])
-        self.assertEqual(stocks.calculator(), 10)
+        self.assertEqual(stocks.max_diff(), 10)
 
-    # def test_if_stockMasta_finds_max2(self):
-    #     stocks = StockMasta([1, 11, 12, 1, 2, 11, 10])
-    #     self.assertEqual(stocks.calculator(), 11)
+    def test_if_stockMasta_finds_max2(self):
+        stocks = StockMasta([1, 11, 12, 1, 2, 11, 10])
+        self.assertEqual(stocks.max_diff(), 11)
 
 
 if __name__ == '__main__':
